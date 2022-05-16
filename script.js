abefba78e1dd7c793357e6af4ead9bc5f76f2a9a
rebecca1990 <93472100+Rebecca1607@users.noreply.github.com>
2022-05-16 12:25:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function clicked(){
+     //find the word ID, take the value of it and store it in wordEntered
+     
+  var wordEntered  = document.getElementById("word").value;
+  console.log(wordEntered)
+
+document.getElementById("box").innerHTML=wordEntered;
+ 
+ }
+ 
+  
